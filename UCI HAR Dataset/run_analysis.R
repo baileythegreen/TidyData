e63bc057eaa02d85811be5d46efff2fa7153761c
baileythegreen <Bailey@le-MacBook-Pro-de-Bailey-Harrington.local>
2015-06-19 21:42:58 +0100

    Repository for Tidy Data Assignment
    This contains the original files from the zip file, a file named
    'tidydata.txt' that contains my tidy data table, and a file named
    'Variable Names.txt' that contains the human readable versions of the
    feature names that were used to name the columns.

---
+++ UCI HAR Dataset/run_analysis.R
@@ -0,0 +1,33 @@
+
+> setwd("/UCI HAR Dataset")
+>               
+> setwd("test")
+>      
+> Xtest <- read.table("X_test.txt")
+> Ytest <- read.table("Y_test.txt")
+> subjecttest <- read.table("subject_test.txt")
+> setwd("/UCI HAR Dataset")
+> setwd("train")
+> Xtrain <- read.table("X_train.txt")
+> Ytrain <- read.table("Y_train.txt")
+> subjecttrain <- read.table("subject_train.txt")
+> 
+> testdata <- cbind(subjecttest, Ytest, Xtest)
+> traindata <- cbind(subjecttrain, Ytrain, Xtrain)
+> 
+> totaldata <- rbind(testdata, traindata)
+> 
+> datasub1 <- totaldata[, c(1:8, 43:48, 83:88, 123:128, 163:168, 203:204, 216:217, 229:230, 242:243, 255:256, 268:273, 347:352, 426:431, 505:506, 518:519, 531:532, 544:545)]
+> setwd("/UCI HAR Dataset")
+> variables <- read.table("Variable Names.txt")
+> colnames(datasub1) <- variables[, 1]
+> 
+> datasub1$Activity[datasub1$Activity == 5] <- "Standing"
+> datasub1$Activity[datasub1$Activity == 1] <- "Walking"
+> datasub1$Activity[datasub1$Activity == 2] <- "WalkingUpstairs"
+> datasub1$Activity[datasub1$Activity == 3] <- "WalkingDownstairs"
+> datasub1$Activity[datasub1$Activity == 4] <- "Sitting"
+> datasub1$Activity[datasub1$Activity == 6] <- "Laying"
+> datasub3 <- aggregate(datasub1[, 3:68], list(Subject = datasub1$Subject, Activity = datasub1$Activity), mean)
+> 
+> write.table(datasub3, "tidydata2.txt", sep="\t", row.names=TRUE, col.names=TRUE)
